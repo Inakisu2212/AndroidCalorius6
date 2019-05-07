be167c9852e28494cadb46566914b732d7642dad
Iñaki Zorrilla García <izorrilla008@ikasle.ehu.eus>
2019-05-07 17:07:47 +0200

    Login a medio acabar. Empezando cambio a activity de reg cal fragment

--- app/src/main/java/com/example/calorius/LoginActivity.java
+++ app/src/main/java/com/example/calorius/LoginActivity.java
@@ -3,67 +3,56 @@ package com.example.calorius;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
-import android.content.pm.PackageManager;
+import android.content.Intent;
 import android.support.annotation.NonNull;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.app.LoaderManager.LoaderCallbacks;
-
-import android.content.CursorLoader;
-import android.content.Loader;
-import android.database.Cursor;
-import android.net.Uri;
-import android.os.AsyncTask;
 
 import android.os.Build;
 import android.os.Bundle;
-import android.provider.ContactsContract;
 import android.text.TextUtils;
-import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.view.inputmethod.EditorInfo;
-import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.Toast;
 
-import java.util.ArrayList;
-import java.util.List;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
-import static android.Manifest.permission.READ_CONTACTS;
 
-/**
- * A login screen that offers login via email/password.
- */
 public class LoginActivity extends AppCompatActivity {
-
-
 
     // Referencias a la UI
     private AutoCompleteTextView mEmail;
     private EditText mPassword;
     private View mProgressView;
     private View mLoginFormView;
+    private Button mEmailSignInButton;
+    private FirebaseAuth mAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        // Set up the login form.
+        // Colocar formulario de logueo
         mEmail = (AutoCompleteTextView) findViewById(R.id.textEmail);
 
-        mPassword = (EditText) findViewById(R.id.password);
+        mPassword = (EditText) findViewById(R.id.textPassword);
 
-        Button mEmailSignInButton = (Button) findViewById(R.id.email_sign_in_button);
+        mEmailSignInButton = (Button) findViewById(R.id.botonLogin);
         mEmailSignInButton.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
                 intentarLogin();
             }
         });
-
+        //Inicializar instancia de firebase auth
+        mAuth = FirebaseAuth.getInstance();
+        //Para animación  de progreso
         mLoginFormView = findViewById(R.id.login_form);
         mProgressView = findViewById(R.id.login_progress);
     }
@@ -108,6 +97,30 @@ public class LoginActivity extends AppCompatActivity {
             // perform the user login attempt.
             showProgress(true);
         }
+    }
+
+    private void LoginUsuario() {
+        String email = mEmail.getText().toString();
+        String password = mPassword.getText().toString();
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Toast.makeText(LoginActivity.this, "Autenticación buena.",
+                                    Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(MainActivity.this,Main2Activity.class));
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
     }
 
     private boolean isEmailValid(String email) {
