7a5e19e6320b77fb3064417af8d2d5de5f3afd16
Iñaki Zorrilla García <izorrilla008@ikasle.ehu.eus>
2019-05-13 21:11:39 +0200

    Nada

--- app/src/main/java/com/example/calorius/regCalActivity.java
+++ app/src/main/java/com/example/calorius/regCalActivity.java
@@ -1,6 +1,7 @@
 package com.example.calorius;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -9,8 +10,11 @@ import android.view.View;
 import android.widget.CalendarView;
 import android.widget.Spinner;
 
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.ValueEventListener;
 
 public class regCalActivity extends AppCompatActivity {
 
@@ -44,7 +48,17 @@ public class regCalActivity extends AppCompatActivity {
         DatabaseReference dbAlimentos = FirebaseDatabase.getInstance()
                 .getReference().child("alimentos");
 
-        //dbAlimentos.
+        dbAlimentos.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
 
     }
 
