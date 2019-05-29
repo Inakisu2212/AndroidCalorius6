72e5f982e77c6dd586a53aa5ddd56499459558f4
ismad <idiez059@ikasle.ehu.eus>
2019-05-29 18:28:16 +0200

    Actualizar Header IN PROGRESS

--- app/src/main/java/com/example/calorius/MainActivity.java
+++ app/src/main/java/com/example/calorius/MainActivity.java
@@ -5,21 +5,37 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.FragmentManager;
-import android.view.View;
-import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    private DatabaseReference dbUsuarios;
+    private ValueEventListener eventListener;
+    String usu="",email,dni,nombre,fotoURL;
+    private TextView usuarioText;
+    private ImageView foto;
+    private SharedPreferences sharedData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,16 +61,63 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        //************* OBTENER SHARED EMAIL USUARIO LOGUEADO *************
+        sharedData = getSharedPreferences("USUARIO", Context.MODE_PRIVATE);
+        email = sharedData.getString(("USUARIO"), "");
+
+        //************* PERSONALIZAR MENÚ USUARIO *************
+        View headerView = navigationView.getHeaderView(0);
+        usuarioText = headerView.findViewById(R.id.textView);
+        foto = headerView.findViewById(R.id.imageView);
+
+        //*********** OBTENER DATOS DEL USUARIO **************
         actualizarHeader();
+
     }
 
     public void actualizarHeader(){
 
-        SharedPreferences spf = getSharedPreferences("shared_prefs", Context.MODE_PRIVATE);
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        View headerView = navigationView.getHeaderView(0);
-        TextView navUsername = (TextView) headerView.findViewById(R.id.textView);
-        navUsername.setText("Hola " + spf.getString("Email",""));
+//        SharedPreferences spf = getSharedPreferences("shared_prefs", Context.MODE_PRIVATE);
+//        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+//        View headerView = navigationView.getHeaderView(0);
+//        TextView navUsername = (TextView) headerView.findViewById(R.id.textView);
+//        navUsername.setText("Hola " + spf.getString("Email",""));
+
+        dbUsuarios = FirebaseDatabase.getInstance().getReference()
+                .child("usuario");
+
+        eventListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+
+                for(DataSnapshot iterador: dataSnapshot.getChildren()) {
+
+                    if(iterador.child("email").getValue().equals(email)){
+                        //************ OBTENER DNI **************
+                        dni = iterador.child("dni").getValue().toString();
+                        //*********** OBTENER FOTO USUARIO **************
+                        fotoURL = iterador.child("foto").getValue().toString();
+                        Glide.with(MainActivity.this)
+                                .load(fotoURL)
+                                .into(foto);
+                        SharedPreferences.Editor editor = sharedData.edit();
+                        editor.putString("DNI", dni);
+                        editor.commit();
+                        usuarioText.setText(iterador.child("email").getValue().toString());
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.e("ERROR", "Error!", databaseError.toException());
+            }
+        };
+
+        dbUsuarios.addValueEventListener(eventListener);
+
 
     }
 
